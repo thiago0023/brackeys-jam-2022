0d709b4592328998eddec9cda676400b3e06e968
rene <renebertoni@gmail.com>
2022-08-27 11:34:02 -0300

    camera find player

--- Assets/_Project/Scripts/Enemies/EnemyActions.cs
+++ Assets/_Project/Scripts/Enemies/EnemyActions.cs
@@ -18,7 +18,6 @@ public class EnemyActions : CharacterSettings
         base.Start();
         if(audioSourceFound)
         {
-            print(audioList[0].audioName);
             AudioHandler.Instance.PlayAudio(audioList[0].audioType, audioList[0].audioName, true, audioSource);
         }
     }
